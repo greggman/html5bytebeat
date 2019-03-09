195eaa5dadc16a1de5d6f09aedc728ef1350b5e5
Gregg Tavares <github@greggman.com>
2019-03-10 00:13:51 +0900

    tweak

--- js/wavmaker.js
+++ js/wavmaker.js
@@ -35,7 +35,7 @@ class WavMaker {
     view.setUint16(20, 1, true);                            // sample format (raw)
     view.setUint16(22, numChannels, true);                  // channel count
     view.setUint32(24, sampleRate, true);                   // sample rate
-    view.setUint32(28, sampleRate * 4, true);               // byte rate (sample rate * block align)
+    view.setUint32(28, sampleRate * numChannels * 2, true); // byte rate (sample rate * block align)
     view.setUint16(32, numChannels * 2, true);              // block align (channel count * bytes per sample)
     view.setUint16(34, 16, true);                           // bits per sample
     writeString(view, 36, 'data');                          // data chunk identifier
