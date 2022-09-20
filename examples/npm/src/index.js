5c4c317354d2d094f17c0e6931b35cca690736cb
Gregg Tavares <github@greggman.com>
2022-09-20 11:21:20 -0700

    add resume

--- examples/npm/src/index.js
+++ examples/npm/src/index.js
@@ -1,6 +1,6 @@
-import ByteBeatNode from "bytebeat.js";
+import ByteBeatNode from 'bytebeat.js';
 
-document.querySelector("button").addEventListener("click", playPause);
+document.querySelector('button').addEventListener('click', playPause);
 
 let g_context;
 let g_byteBeatNode;
@@ -8,12 +8,13 @@ let g_playing = false;
 
 async function init() {
   g_context = new AudioContext();
+  g_context.resume();  // needed for safari
   await ByteBeatNode.setup(g_context);
   g_byteBeatNode = new ByteBeatNode(g_context);
   g_byteBeatNode.setType(ByteBeatNode.Type.byteBeat);
   g_byteBeatNode.setExpressionType(ByteBeatNode.ExpressionType.infix);
   g_byteBeatNode.setDesiredSampleRate(8000);
-  g_byteBeatNode.setExpressions(["((t >> 10) & 42) * t"]);
+  g_byteBeatNode.setExpressions(['((t >> 10) & 42) * t']);
 }
 
 async function playPause() {
