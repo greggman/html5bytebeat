eedfad7d25047df661513da8a9cf2f9c5459cc51
Gregg Tavares <github@greggman.com>
2022-09-20 22:21:26 -0700

    allow passing in a test file to update-songs

--- build/update-songs.js
+++ build/update-songs.js
@@ -3,13 +3,28 @@ const { Octokit } = require('octokit');
 const fs = require('fs');
 const path = require('path');
 
-async function main() {
+async function getComments() {
+  if (process.argv[2]) {
+    const body = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+    return [
+      {
+        body,
+        reactions: {},
+        user: {
+          id: 123,
+          login: 'gman',
+        },
+      },
+    ];
+  }
+
   const token = process.env.GH_TOKEN;
   if (!token) {
     console.error('must set GH_TOKEN');
     // eslint-disable-next-line no-process-exit
     process.exit(1);
   }
+
 
   const octokit = new Octokit({
     auth: token,
@@ -20,6 +35,11 @@ async function main() {
     repo: 'html5bytebeat',
     issue_number: '17',
   });
+  return comments;
+}
+
+async function main() {
+  const comments = await getComments();
 
   const linkRE = /\[(.*?)\]\((https:\/\/greggman\.com\/downloads\/examples\/html5bytebeat\/html5bytebeat\.html#.*?)\)/g;
   const songs = [];
